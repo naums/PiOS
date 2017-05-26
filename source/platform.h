b2ba148e10e85e9ebb8c7dc9b3aa97b393c4bd0d
Stefan Naumann <me@stefannaumann.de>
2017-05-26 22:03:16 +0200

    documented platform.h

--- source/platform.h
+++ source/platform.h
@@ -7,13 +7,19 @@
     //#define PLATFORM_RPI2
 
     #ifdef PLATFORM_RPI
+        /// the base address of the periphals in memory
         #define PBASE 0x20000000
+        /// name of the "board" for writing in human readable name
         #define PNAME "Raspberry Pi"
     #elif PLATFORM_RPIBP
+        /// the base address of the periphals in memory
         #define PBASE 0x20000000
+        /// name of the "board" for writing in human readable name
         #define PNAME "Raspberry Pi Model B+"
     #elif defined PLATFORM_RPI2
+        /// the base address of the periphals in memory
         #define PBASE 0x3F000000
+        /// name of the "board" for writing in human readable name
         #define PNAME "Raspberry Pi 2"
     #endif
 #endif

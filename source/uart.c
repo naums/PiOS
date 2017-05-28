37cc0c2fee47b87a43f8d5f71b3ebb87ed57e5f0
Stefan Naumann <me@stefannaumann.de>
2017-05-28 13:36:53 +0200

    removed gpio-constants from uart.c

--- source/uart.c
+++ source/uart.c
@@ -1,15 +1,6 @@
 #include "uart.h"
 
 volatile uint32_t* const pios_aux = (volatile uint32_t* const) (PBASE + AUX_BASE_ADDR);
-void dummy ( unsigned int );
-
-
-// TODO: Gpio.s
-#define GPFSEL1         (PBASE+0x00200004)
-#define GPSET0          (PBASE+0x0020001C)
-#define GPCLR0          (PBASE+0x00200028)
-#define GPPUD           (PBASE+0x00200094)
-#define GPPUDCLK0       (PBASE+0x00200098)
 
 void pios_uart_init ( )
 {    
